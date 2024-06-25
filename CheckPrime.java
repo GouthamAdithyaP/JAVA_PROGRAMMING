d07e45ce9d162d0a7bdf9428bd4f2bc5f2cfad17
GouthamAdithyaP <142380000+GouthamAdithyaP@users.noreply.github.com>
2024-06-25 21:08:02 +0530

    Create CheckPrime.java

---
+++ CheckPrime.java
@@ -0,0 +1,30 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class question8 {
+
+    public static boolean checkPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        if (checkPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+        scanner.close();
+    }
+}
