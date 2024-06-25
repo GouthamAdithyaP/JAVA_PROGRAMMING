8dda27cc93a9ddad7893d0087d22f0ed7985afd4
GouthamAdithyaP <142380000+GouthamAdithyaP@users.noreply.github.com>
2024-06-25 21:07:38 +0530

    Create CheckLeapYear.java

---
+++ CheckLeapYear.java
@@ -0,0 +1,23 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class question7 {
+
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a year: ");
+        year = scanner.nextInt();
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+        scanner.close();
+    }
+}
